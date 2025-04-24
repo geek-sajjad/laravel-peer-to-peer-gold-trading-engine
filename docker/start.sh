4ec9468afa99472585fa94d2a95c06b5f6ec609f
Sajad Sohrabi <sajjad.sohrabi2@gmail.com>
2025-04-24 08:55:44 +0000

    update start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -8,18 +8,35 @@ cp .env.example .env
 
 
 echo "🚀 Building containers..."
-docker compose up -d --build
+
+docker compose up -d --build app webserver db
+
+
 
 echo "🔐 Running migrations..."
 docker exec -it laravel-app php artisan migrate --force || true
 
+
 echo "🔐 Running seeders ..."
 docker exec -it laravel-app php artisan db:seed --force || true
 
-# echo "🧹 Setting file permissions..."
-# sudo chown -R $USER:$USER .
+
+#echo "🔐 Running api  install..."
+#docker exec -it laravel-app php artisan install:api --force || true
+
+
+echo "🚀 Building queue container..."
+docker compose up -d --build queue
+
+#echo "Waiting for services to be ready..."
+#sleep 10 # Adjust sleep time if needed to ensure DB is ready
+
+
+echo "🧹 Setting file permissions..."
+sudo chown -R $USER:$USER .
 
 
 echo "✅ Laravel is ready at: http://localhost:8000"
+
 
 
